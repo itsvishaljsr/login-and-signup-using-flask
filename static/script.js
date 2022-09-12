c2961bc9dc9d340e248f76d11f3ea0db6445526e
Vishal Prajapati <vishalprajapatijsr@gmail.com>
2022-09-12 19:26:48 +0530

    code in the files are changed

--- static/script.js
+++ static/script.js
@@ -1,5 +1,14 @@
-var check = function() {
+function checkPasswordLength(password){
+        if (password.length < 8 || password.length>16) {
+            document.getElementById("errorSpan").innerHTML = "Password length must be greater than 8 and less than 16";
+            document.getElementById("signupbtn").disabled = true;
+        }
+        else {
+            document.getElementById("errorSpan").innerHTML = "";
+        } 
+   }
 
+var check = function() {
     if (document.getElementById('password').value == document.getElementById('checkPassword').value) {
             document.getElementById("signupbtn").disabled = false;
             document.getElementById('signupbtn').style.backgroundColor = '#3498db';
